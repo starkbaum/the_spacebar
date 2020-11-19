c194c8c96e3ddb44e1c3681b0b49ca5b563be200
starkbaum <s.starkbaum@austrian-standards.at>
2020-11-19 13:08:30 +0100

    Chapter 3: routes, controllers, pages

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleController
+{
+    /**
+     * @Route("/")
+     * @return Response
+     */
+    public function homepage()
+    {
+        return new Response('OMG!');
+    }
+
+    /**
+     * @Route("/news/{slug}")
+     */
+    public function show($slug)
+    {
+        return new Response(sprintf(
+            'Future page to show the article: %s',
+            $slug
+        ));
+    }
+}
